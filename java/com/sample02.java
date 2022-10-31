f1035dca5ea63f7b5bafdd9929083004d3ce5ec4
Wang Yu <ousannjp@gmail.com>
2022-10-31 16:20:02 +0900

    Add java File 02

---
+++ java/com/sample02.java
@@ -0,0 +1,15 @@
+public class Main {
+ 
+    public static void main(String[] args) throws Exception {
+ 
+        int age = 25;
+ 
+        // 条件分岐処理
+        if (age > 20) {
+            System.out.println("あなたの年齢は20歳以上ですね！");
+        }
+        else {
+            System.out.println("あなたの年齢は20歳以上ではありません");
+        }
+    }
+}
