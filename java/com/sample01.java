299fb8c6f46e0a108217315699820730c16abadf
Wang Yu <ousannjp@gmail.com>
2022-10-31 16:10:10 +0900

    Add java file

---
+++ java/com/sample01.java
@@ -0,0 +1,15 @@
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+        int[] score = {10, 20, 30, 40, 50};
+        
+        System.out.println(score[0]);
+        System.out.println(score[1]);
+        System.out.println(score[2]);
+        System.out.println(score[3]);
+        System.out.println(score[4]);
+            
+    }
+ 
+}
